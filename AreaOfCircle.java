f9c62a35d86b7e84e2893300b05cdcaa00b6eb4a
Shanice Hopson <shanice.l.hopson@students.jsums.edu>
2017-12-16 01:28:00 -0600

    Update AreaOfCircle.java

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -2,23 +2,20 @@
 Hopson, Shanice
 */
 
-public class AreaOfCircle
-{
+    public class AreaOfCircle
+    {
     public static void main (String[]args)
     {  
-        
-    //define the data we know
         final double PI = 3.14159;
-    
-//define other data we will use
-    double radius;
-    double area;
-//give radius a value
-    radius = 4.6;
-//perform the calculation
-    area = PI* radius* radius;
-//output the results
-    System.out.println("The area of the circle is" + area );
+        double radius;
+        double area;
+        radius = 4.6;
+
+        //perform the calculation
+        area = PI* radius* radius;
+        
+        //output the results
+        System.out.println("The area of the circle is" + area );
     }
 }
 
